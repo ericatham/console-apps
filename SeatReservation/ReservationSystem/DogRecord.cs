6f1671a8ee2d33269949abbd38eead89b57725b8
Erica Menezes <ericat.menezes@gmail.com>
2018-02-09 17:59:49 -0500

    add Seat Reservation app

---
+++ SeatReservation/ReservationSystem/DogRecord.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4EricaTdeSMenezesP1
+{
+    //Class created to store the informations about the dog and the methods to manage these informations
+    class DogRecord
+    {
+        //creation of attributes private
+        private string Name;
+        private string Breed;
+        private string Colour;
+        private string Gender;
+
+        //default constructor to initialize the fields 
+        public DogRecord()
+        {
+            Name = null;
+            Breed = null;
+            Colour = null;
+            Gender = null;
+        }
+
+        //non-default constructor 
+        public DogRecord(string name, string breed, string colour, string gender)
+        {
+            Name = name;
+            Breed = breed;
+            Colour = colour;
+            Gender = gender;
+        }
+        public void DisplayDogInformation()
+        {
+            if (Name == null && Breed == null && Colour == null && Gender == null)//check if there is any record
+            {
+                Console.WriteLine("No dog record exists");
+                Console.ReadLine();
+            }
+            else
+            {
+                //displaying the information, if there is any record
+                Console.WriteLine("Name:" + Name);
+                Console.WriteLine("Breed:" + Breed);
+                Console.WriteLine("Colour:" + Colour);
+                Console.WriteLine("Gender:" + Gender);
+                Console.ReadLine();
+            }
+        }
+        public void EditDogInformation()//method to edit the informations in the attributes
+        {
+            bool condition = true;
+
+            if (Name == null && Breed == null && Colour == null && Gender == null)
+            {
+                Console.WriteLine("No dog record exists");//check if there is any record
+                Console.ReadLine();
+            }
+            else
+            {
+                try
+                {
+                    
+                    Console.WriteLine("The current values are:");
+                    DisplayDogInformation();//Display the information that was in the record using the method
+                    Console.WriteLine("Please, enter the new name:");//asks for the new information
+                    Name = Console.ReadLine();
+                    Console.WriteLine("Please, enter the new breed:");
+                    Breed = Console.ReadLine();
+                    Console.WriteLine("Please, enter the new colour:");
+                    Colour = Console.ReadLine();
+                    do //loop to keep asking the user to insert the appropriate data
+                    {
+                        Console.WriteLine("Please, enter the new gender:");
+                        Gender = Console.ReadLine();
+                        if (Gender == "male" || Gender == "MALE" || Gender == "Male" || Gender == "female" || Gender == "FEMALE" || Gender == "Female")
+                        {
+                            condition = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. You should enter 'male' or 'female'");
+                        }
+                    } while (condition);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.ReadLine();
+                }
+            }
+        }
+            public void AddDog()//method to add information about a dog
+        {
+            try
+            {
+                bool condition = true;
+
+                if (Name != null && Breed != null && Colour != null && Gender != null)//check if there is already a record and display it
+                {
+                    Console.WriteLine("Dog record already exists");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Enter the name of the dog:");
+                    Name = Console.ReadLine();
+                    Console.WriteLine("Enter the breed of the dog:");
+                    Breed = Console.ReadLine();
+                    Console.WriteLine("Enter the colour of the dog:");
+                    Colour = Console.ReadLine();
+                    do
+                    {
+                        Console.WriteLine("Enter the gender of the dog:");
+                        Gender = Console.ReadLine();
+                        if (Gender == "male" || Gender == "MALE" || Gender == "Male" || Gender == "female" || Gender == "FEMALE" || Gender == "Female")
+                        {
+                            condition = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. You should enter 'male' or 'female'");
+                        }
+                    } while (condition);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input");
+                Console.ReadLine();
+            }
+            }
+        }
+    }
+     
+
---
+++ SeatReservation/ReservationSystem/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4EricaTdeSMenezesP1
+{
+    class Program
+    {
+        //Program to add, edit and display informations of a dog
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool condition;
+                string choice;
+                DogRecord dog; //using the class "DogRecord" created
+
+                //Creating of the class
+                dog = new DogRecord();
+                condition = true;
+
+                //looping to keep displaying the menu, unless asked exit is required
+                do
+                {
+                    Console.WriteLine("Select one of the options: [a]Display dog information; [b]Add a dog; [c]Edit an existing dog; [d]Exit the program;");
+                    choice = Console.ReadLine();
+                    switch (choice)
+                    {
+
+                        case "a":
+                            dog.DisplayDogInformation(); //calls the method of the class created to display the attributes
+                            break;
+                        case "b":
+                            dog.AddDog(); //calls the method of the class created to add values to attributes
+                            break;
+                        case "c":
+                            dog.EditDogInformation(); //calls the method of the class used to edit the attributes
+                            break;
+                        case "d":
+                            condition = false; //ends the looping assigning a false value to variable "condition"
+                            break;
+                        default:
+                            Console.WriteLine("You did not insert any of the options required. Try again!");
+                            Console.ReadLine();
+                            break;
+                    }
+                } while (condition);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You inserted an invalid type of data!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid data");
+            }
+        }
+    }
+}
